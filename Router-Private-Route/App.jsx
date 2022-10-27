404541652dc4b96ea273594f0166934c44404123
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-10-27 22:37:25 +0300

    Added routing demo - private/public

---
+++ Router-Private-Route/App.jsx
@@ -0,0 +1,37 @@
+import { Routes, Route, Navigate, useNavigate, useLocation } from 'react-router-dom';
+
+import { history } from '_helpers';
+import { Nav, PrivateRoute } from '_components';
+import { Home } from 'home';
+import { Login } from 'login';
+
+export { App };
+
+function App() {
+    // The /login route is public
+    // the home route (/) is protected by the private route component 
+    // - uses Redux to check if the user is logged in.
+
+    history.navigate = useNavigate();
+    history.location = useLocation();
+
+    return (
+        <div className="app-container bg-light">
+            <Nav />
+            <div className="container pt-4 pb-4">
+                <Routes>
+                <Route
+                        path="/"
+                        element={
+                            <PrivateRoute>
+                                <Home />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+            </div>
+        </div>
+    );
+}
